a2ee2619990f7ede330bdc98f3bedf72a3dfe4f7
Pavel Melnikov <PavelNMel@yandex.ru>
2022-07-24 22:17:10 +0300

    feature: на проверку

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import buttonEdit from '../image/button-edit.svg';
 import buttonAdd from '../image/add-button.svg';
 import Card from './Card.js';
 import { CurrentUserContext } from '../context/CurrentUserContext.js';
-import { CardContext } from '../context/CardContext.js';
+
 
 function Main({onAddPlace, onEditProfile, onEditAvatar, cardClick, cards, onCardLike, onCardDelete}) {
     
@@ -14,49 +14,47 @@ function Main({onAddPlace, onEditProfile, onEditAvatar, cardClick, cards, onCard
     }
 
     return (
-        <CardContext.Provider value={cards}>
-            <main className="main">
-                <section className="profile">
-                    <div className="profile__item">
-                        <a
-                            className="profile__avatar"
-                            onClick={onEditAvatar}
-                            style={{ backgroundImage: `url(${currentUser.avatar})` }}
-                        ></a>
-                        <div className="profile__info">
-                            <div className="profile__info-item">
-                                <h1 className="profile__info-name">{currentUser.name}</h1>
-                                <button className="profile__edit-button" type="button" onClick={onEditProfile}>
-                                    <img className="profile__edit-icon" src={buttonEdit} alt="Кнопка редактирования" />
-                                </button>
-                            </div>
-                            <p className="profile__info-about">{currentUser.about}</p>
+        <main className="main">
+            <section className="profile">
+                <div className="profile__item">
+                    <a
+                        className="profile__avatar"
+                        onClick={onEditAvatar}
+                        style={{ backgroundImage: `url(${currentUser.avatar})` }}
+                    ></a>
+                    <div className="profile__info">
+                        <div className="profile__info-item">
+                            <h1 className="profile__info-name">{currentUser.name}</h1>
+                            <button className="profile__edit-button" type="button" onClick={onEditProfile}>
+                                <img className="profile__edit-icon" src={buttonEdit} alt="Кнопка редактирования" />
+                            </button>
                         </div>
+                        <p className="profile__info-about">{currentUser.about}</p>
                     </div>
-                    <button className="profile__add-button" type="button" onClick={onAddPlace} >
-                        <img className="profile__add-image" src={buttonAdd} alt="Кнопка добавления" />
-                    </button>
-                </section>
-                
-                <ul className="element">
-                    {cards.map((item) => {
-                        return (
-                            <Card
-                                key={item._id}
-                                cardId={item._id}
-                                name={item.name}
-                                link={item.link}
-                                likeUser={item.likes}
-                                like={item.likes.length}
-                                ownerId={item.owner._id}
-                                onCardClick={getCardsClick}
-                                onCardLike={onCardLike}
-                                onCardDelete={onCardDelete}
-                            />)
-                    })}
-                </ul>
-            </main>
-        </CardContext.Provider>
+                </div>
+                <button className="profile__add-button" type="button" onClick={onAddPlace} >
+                    <img className="profile__add-image" src={buttonAdd} alt="Кнопка добавления" />
+                </button>
+            </section>
+            
+            <ul className="element">
+                {cards.map((item) => {
+                    return (
+                        <Card
+                            key={item._id}
+                            cardId={item._id}
+                            name={item.name}
+                            link={item.link}
+                            likeUser={item.likes}
+                            like={item.likes.length}
+                            ownerId={item.owner._id}
+                            onCardClick={getCardsClick}
+                            onCardLike={onCardLike}
+                            onCardDelete={onCardDelete}
+                        />)
+                })}
+            </ul>
+        </main>
     )
 };
 
